e9d79c6b0d4da7dc7e67386438ba0a1621287a5c
Alan <alanstos@gmail.com>
2024-07-27 22:19:59 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Caro, amigo. A terra é redonda. Acredite! =D assista: https://www.youtube.com/watch?v=Eup5uWLvhqw</h1>
+function Home() {
+  return (
+    <h1>
+      Caro, amigo. A terra é redonda. Acredite! =D assista:
+      https://www.youtube.com/watch?v=Eup5uWLvhqw
+    </h1>
+  );
 }
 
 export default Home;
