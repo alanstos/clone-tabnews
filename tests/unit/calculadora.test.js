3ab9ce1d528a6b4f2efdb0c23c8ed96b636215e3
Alan <alanstos@gmail.com>
2024-08-04 21:01:11 +0000

    reorganizacao pasta e subpastas `tests`

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,4 +1,4 @@
-const calculadora = require("../models/calculadora");
+const calculadora = require("../../models/calculadora");
 
 test("espero somar 2 + 2 igual a 4", () => {
   const resultado = calculadora.somar(2, 2);
