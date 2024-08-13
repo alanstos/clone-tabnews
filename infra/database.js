da6d05b5994c6bf39fe236ae7c4afc826bcbb647
Alan <alanstos@gmail.com>
2024-08-13 01:20:22 +0000

    add ssl connect at postgres

--- infra/database.js
+++ infra/database.js
@@ -8,7 +8,9 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
+  console.log(process.env.NODE_ENV);
   console.log("Credencias database:", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
