2616bdbfd55adc62ed36efa840a8eb50d04a73d1
Alan <alanstos@gmail.com>
2024-08-30 03:24:00 +0000

    correcao bug migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,35 +3,45 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-
-  const migrationDefaultOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-
-  if (request.method === "GET") {
-    const pendingMigration = await migrationRunner(migrationDefaultOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigration);
-  }
-
-  if (request.method === "POST") {
-    const migrationExecuted = await migrationRunner({
-      ...migrationDefaultOptions,
-      dryRun: false,
+  const methodAllowed = ["GET", "POST"];
+  if (!methodAllowed.includes(request.method)) {
+    return response.status(405).json({
+      error: `method ${request.method} not allowed`,
     });
-    await dbClient.end();
-
-    if (migrationExecuted.length > 0) {
-      return response.status(201).json(migrationExecuted);
-    }
-    return response.status(200).json(migrationExecuted);
   }
 
-  return response.status(405).end();
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migrationDefaultOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
+    if (request.method === "GET") {
+      const pendingMigration = await migrationRunner(migrationDefaultOptions);
+      return response.status(200).json(pendingMigration);
+    }
+
+    if (request.method === "POST") {
+      const migrationExecuted = await migrationRunner({
+        ...migrationDefaultOptions,
+        dryRun: false,
+      });
+
+      if (migrationExecuted.length > 0) {
+        return response.status(201).json(migrationExecuted);
+      }
+      return response.status(200).json(migrationExecuted);
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await dbClient.end();
+  }
 }
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -28,3 +28,11 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(data2)).toBe(true);
   expect(data2.length).toBe(0);
 });
+
+test("DELETE to /api/v1/migrations should return 405", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "DELETE",
+  });
+  const data1 = await response.json();
+  expect(response.status).toBe(405);
+});
