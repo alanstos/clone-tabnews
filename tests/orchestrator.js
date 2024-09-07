e176eea8f8795cfcbf0595d894dd5b0104f5c258
Alan <alanstos@gmail.com>
2024-09-07 22:09:48 +0000

    estabilizar `npm test` orquestrador

---
+++ tests/orchestrator.js
@@ -0,0 +1,21 @@
+const retry = require("async-retry");
+
+async function waitForAllServices() {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    return await retry(
+      async () => {
+        const response = await fetch("http://localhost:3000/api/v1/status");
+        const body = await response.json();
+      },
+      {
+        retries: 100,
+      },
+    );
+  }
+}
+
+export default {
+  waitForAllServices,
+};
