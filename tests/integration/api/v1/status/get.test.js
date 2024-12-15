e2af095f96742458e27b568d555e4fee0e0c98e2
Alan <alanstos@gmail.com>
2024-12-15 21:37:04 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,13 +4,20 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
-  const data = await response.json();
-  const parseUpdatedAt = new Date(data.updated_at).toISOString();
-  expect(data.updated_at).toEqual(parseUpdatedAt);
-  expect(data.dependencies.database.version).toEqual("16.3");
-  expect(data.dependencies.database.max_connections).toEqual(100);
-  expect(data.dependencies.database.opened_connections).toEqual(1);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
+
+      const data = await response.json();
+
+      const parseUpdatedAt = new Date(data.updated_at).toISOString();
+
+      expect(data.updated_at).toEqual(parseUpdatedAt);
+      expect(data.dependencies.database.version).toEqual("16.3");
+      expect(data.dependencies.database.max_connections).toEqual(100);
+      expect(data.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
