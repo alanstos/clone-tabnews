7c6d64b1621733ea598c235b9d70267ea9d08c15
Alan <alanstos@gmail.com>
2025-01-29 18:33:40 +0000

    feat: melhoria tela de `/status`

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,13 +1,46 @@
-function CapsLock(propriedades) {
-  console.log("valorCapsLock=", propriedades);
-  return propriedades.texto.toUpperCase();
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const data = await response.json();
+  return data;
 }
 
 export default function StatusPage() {
   return (
     <>
-      <h1>page status page</h1>
-      <CapsLock texto="teste valor texto" valor="10" />
+      <h1>STATUS PAGE</h1>
+      <UpdatedAt />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { data, error, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 5000,
+    dedupingInterval: 2000, //manter padrao
+  });
+
+  if (isLoading) return <div>Carregando...</div>;
+
+  const updateAtTexto = new Date(data.updated_at).toLocaleString("pt-BR");
+
+  return (
+    <>
+      <p>
+        <em>Ultima atualização: {updateAtTexto}</em>
+      </p>
+      <p>
+        <em>Versão: {data.dependencies.database.version}</em>
+      </p>
+      <p>
+        <em>Conexões máximas: {data.dependencies.database.max_connections}</em>
+      </p>
+      <p>
+        <em>
+          Qtd conexões abertas: {data.dependencies.database.opened_connections}
+        </em>
+      </p>
     </>
   );
 }
