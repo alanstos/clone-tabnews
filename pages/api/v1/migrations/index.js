010f6c5c838ec4f206df1e6513a751d9816ac113
Alan <alanstos@gmail.com>
2025-09-03 01:41:42 +0000

    chore: lint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,7 +1,6 @@
 import migrationRunner from "node-pg-migrate";
 import { resolve } from "node:path";
 import database from "infra/database";
-import { InternalServerError, MethodNotAllwedError } from "infra/errors";
 import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
 
