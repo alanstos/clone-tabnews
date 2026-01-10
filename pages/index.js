a746ec99a05cb4d7dda97fd114d665d09f6544ee
Alan Araujo <alanstos@gmail.com>
2026-01-10 17:32:07 -0300

    fix: home is defined but never used

--- pages/index.js
+++ pages/index.js
@@ -56,23 +56,4 @@ function Main(){
   )
 }
 
-function Home() {
-  return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-2xl font-bold mb-4 text-center">
-        Caro amigo, a Terra é redonda. Acredite! =D
-      </h1>
-      <div className="w-full max-w-xl aspect-video">
-        <iframe
-          className="w-full h-full rounded-xl shadow-lg"
-          src="https://www.youtube.com/embed/Eup5uWLvhqw"
-          title="YouTube video"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
-      </div>
-    </main>
-  );
-}
-
 export default Main;
